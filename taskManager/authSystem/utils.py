5e1a1799dea0b4f44cfdc229a8f49229f7510440
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-07 14:53:14 +0200

    update

---
+++ taskManager/authSystem/utils.py
@@ -0,0 +1,10 @@
+import re
+
+def check_email(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    # pass the regular expression
+    # and the string into the fullmatch() method
+    if (re.fullmatch(regex, email)):
+        return True
+    else:
+        return False
--- taskManager/authSystem/views.py
+++ taskManager/authSystem/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from .utils import check_email
 # Create your views here.
+
+
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html', {'form': RegisterForm()})
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 == password2:
+            username_taken = User.objects.filter(username=username).exists()
+            email_taken = User.objects.filter(email=email).exists()
+
+            if username_taken:
+                error = 'This username is taken. Try again!'
+            if email_taken:
+                error = 'This email is taken. Try again!'
+            
+            if not username_taken and not email_taken:
+                email_valid = check_email(email)
+                if email_valid:
+                    pass
+                else:
+                    error = 'Invalid email. Try again!'
+
+        else:
+            error = 'Passwords did not match. Try again!'
+
+        return render(request, 'register.html', {'form': RegisterForm(), 'error': error})
